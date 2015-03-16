f022c0e9c562641563d0267c5533cc35c72df1ca
tawnos84 <kai.zimmer@gmail.com>
2015-03-16 22:23:23 +0000

    Passing search parameters on to search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,12 @@
 class SearchesController < ApplicationController
   def index
     @menus = Menu.all
+
+    #save search parameters
+    @search = Search.new
+    @search.cook_location = params[:search_location]
+    @search.cook_date = params[:search_date]
+    @search.cook_time = params[:search_time]
   end
 
   def new
@@ -8,8 +14,8 @@ class SearchesController < ApplicationController
   end
 
   def create
-
-    redirect_to searches_path
+    @search = Search.new(search_params)
+    redirect_to searches_path(search_location: @search.cook_location, search_date: @search.cook_date, search_time: @search.cook_time)
   end
 
   def ask
@@ -17,5 +23,11 @@ class SearchesController < ApplicationController
   end
 
   def result
+  end
+
+  private
+  def search_params
+    params.require(:search).permit(
+        :cook_location, :cook_time, :cook_date)
   end
 end
