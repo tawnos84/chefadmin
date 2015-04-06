343e5a02d80edaf0cc32521972599e8436298601
tawnos84 <kai.zimmer@gmail.com>
2015-04-06 22:44:07 +0100

    User Login works without remembering

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Thank you for signing up and Welcome to LoSibaritas!"
       redirect_to @user
     else
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+
   get 'signup' => 'users#new'
 
   get 'users/edit'
