5e4998696e52391af1cc9e90218fd443e26fa21a
tawnos84 <kai.zimmer@gmail.com>
2015-04-07 10:44:45 +0100

    Made sessions persistent with cookies

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150406122422) do
+ActiveRecord::Schema.define(version: 20150407091214) do
 
   create_table "chefs", force: :cascade do |t|
     t.string   "name"
@@ -130,6 +130,7 @@ ActiveRecord::Schema.define(version: 20150406122422) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "remember_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
