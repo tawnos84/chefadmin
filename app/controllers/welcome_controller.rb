f75a8f8b5db98f7b2c934376206b574a69b66e9e
tawnos84 <kai.zimmer@gmail.com>
2015-04-09 17:11:54 +0100

    Chef dashboard done

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,10 +5,7 @@ class WelcomeController < ApplicationController
     @chef = Chef.find_by_email(current_user.email)
     @menus = @chef.menus.all
     @orders = Order.all.where(:chef_id => @chef.id)
-    if @orders.find_by(:status => 'PENDING')
-      message = "You have pending orders! Check them out in the navigation"
-      flash[:warning] = message
-    end
+
   end
 
   def admin
