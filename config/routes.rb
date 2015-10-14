ac1387fee4bf9663577d0365b3583c887ba495c6
tawnos84 <kai.zimmer@gmail.com>
2015-10-14 20:51:34 +0100

    prep for launch

--- config/routes.rb
+++ config/routes.rb
@@ -95,7 +95,7 @@ Rails.application.routes.draw do
   resources :subscribers
 
   # You can have the root of your site routed with "root"
-  root 'events#cheflanding'
+  root 'searches#ask'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
